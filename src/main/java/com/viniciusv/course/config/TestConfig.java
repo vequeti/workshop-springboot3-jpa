a1fc2bd50d1a2611b6e6c367795c2485c49f2366
Vincius Vequeti <viniciusvequeti@gmail.com>
2023-03-10 11:06:17 -0300

    Payment, one-to-one association

--- src/main/java/com/viniciusv/course/config/TestConfig.java
+++ src/main/java/com/viniciusv/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.viniciusv.course.entities.Category;
 import com.viniciusv.course.entities.Order;
 import com.viniciusv.course.entities.OrderItem;
+import com.viniciusv.course.entities.Payment;
 import com.viniciusv.course.entities.Product;
 import com.viniciusv.course.entities.User;
 import com.viniciusv.course.entities.enums.OrderStatus;
@@ -78,5 +79,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
