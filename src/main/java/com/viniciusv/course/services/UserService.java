eb430366e357225b9b01afe92ee33b7dd9dbeb43
Vincius Vequeti <viniciusvequeti@gmail.com>
2023-03-14 14:03:14 -0300

    Exception handling - delete

--- src/main/java/com/viniciusv/course/services/UserService.java
+++ src/main/java/com/viniciusv/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.viniciusv.course.entities.User;
 import com.viniciusv.course.repositories.UserRepository;
+import com.viniciusv.course.services.exceptions.DatabaseException;
 import com.viniciusv.course.services.exceptions.ResourceNotFoundException;
 
 
@@ -31,7 +34,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
